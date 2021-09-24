97fc6a096692fa2df320b5fc32829f655b96b70f
hawoond <hawoond@gmail.com>
2021-09-24 18:56:11 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace WindowsFormsAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInputNum = new System.Windows.Forms.TextBox();
+            this.lbGuide = new System.Windows.Forms.Label();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btInput = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbInputNum
+            // 
+            this.tbInputNum.Location = new System.Drawing.Point(32, 60);
+            this.tbInputNum.Name = "tbInputNum";
+            this.tbInputNum.Size = new System.Drawing.Size(199, 25);
+            this.tbInputNum.TabIndex = 0;
+            // 
+            // lbGuide
+            // 
+            this.lbGuide.AutoSize = true;
+            this.lbGuide.Location = new System.Drawing.Point(29, 38);
+            this.lbGuide.Name = "lbGuide";
+            this.lbGuide.Size = new System.Drawing.Size(336, 19);
+            this.lbGuide.TabIndex = 1;
+            this.lbGuide.Text = "1부터 100사이의 숫자를 입력해 주세요";
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(29, 143);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(145, 15);
+            this.lbResult.TabIndex = 2;
+            this.lbResult.Text = "입력한 숫자의 합은?";
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(194, 140);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(100, 25);
+            this.tbResult.TabIndex = 3;
+            // 
+            // btInput
+            // 
+            this.btInput.Location = new System.Drawing.Point(237, 62);
+            this.btInput.Name = "btInput";
+            this.btInput.Size = new System.Drawing.Size(75, 23);
+            this.btInput.TabIndex = 4;
+            this.btInput.Text = "입력";
+            this.btInput.UseVisualStyleBackColor = true;
+            this.btInput.Click += new System.EventHandler(this.btInput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 199);
+            this.Controls.Add(this.btInput);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.lbGuide);
+            this.Controls.Add(this.tbInputNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInputNum;
+        private System.Windows.Forms.Label lbGuide;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btInput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            btInput.Click += btInput_Click;
+        }
+        //숫자 입력 후 버튼 클릭 시 입력한 텍스트를 가지고 Total 함수를 실행한다.
+        private void btInput_Click(object sender, EventArgs e)
+        {
+            string result = tbInputNum.Text;
+
+            //입력한 텍스트(number)를 배열로 변환
+            char[] arr;
+            arr = result.ToCharArray();
+            
+            Total(arr);
+        }
+       
+        
+        private void Total(char[] numbers)
+        {
+            //변환한 배열을 숫자형식의 리스트로 변경 
+            int Sum = new int();
+            Sum = 0;
+
+            //인덱스 카운트만큼 포문을 돌려 합한다
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                //char형식으로 했더니 아스키코드로 나옴.
+                // 문자열로 변환한 다음 다시 숫자로 변환해서 합해준다.
+                Sum = Sum + int.Parse(numbers[i].ToString());
+            }
+            tbResult.Text = Sum.ToString();
+        }
+        
+        
+    }
+}
